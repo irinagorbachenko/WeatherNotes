ad6954e6d1ef86d3452ec3ef376bd53db361b895
Irina Gorbachenko <irinagorbachenko23@gmail.com>
2025-11-17 14:36:49 +0200

    Initial Commit

---
+++ WeatherNotes/WeatherNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherNotesApp.swift
+//  WeatherNotes
+//
+//  Created by Irina Gorbachenko on 17.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherNotesTests/WeatherNotesTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherNotesTests.swift
+//  WeatherNotesTests
+//
+//  Created by Irina Gorbachenko on 17.11.2025.
+//
+
+import Testing
+@testable import WeatherNotes
+
+struct WeatherNotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
