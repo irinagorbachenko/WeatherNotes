9806642ea39da86505d5cb286ecc2040f426dd67
Irina Gorbachenko <irinagorbachenko23@gmail.com>
2025-11-17 21:23:13 +0200

    Moke notes for view is created

--- WeatherNotes/NotesListScreen/ViewModels/NoteListViewModel.swift
+++ WeatherNotes/NotesListScreen/ViewModels/NoteListViewModel.swift
@@ -8,5 +8,9 @@ import SwiftUI
 
 
 class NoteListViewModel: ObservableObject {
-    @Published var notes: [Note] = []
+    @Published var notes: [Note] = [
+        Note(id: UUID(),title: "Walking", createdAt: Date(), temperature: 18, icon: nil),
+        Note(id: UUID(),title: "Running", createdAt: Date(), temperature: 24, icon: nil),
+        Note(id: UUID(),title: "Park", createdAt: Date(), temperature: 16, icon: nil)
+    ]
 }
