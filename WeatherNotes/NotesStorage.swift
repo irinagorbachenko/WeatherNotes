2ec44db7c5f4692bf15b0c8d732b080a508b435c
Irina Gorbachenko <irinagorbachenko23@gmail.com>
2025-11-17 19:21:39 +0200

    UserDefaults files is created

---
+++ WeatherNotes/NotesStorage.swift
@@ -0,0 +1,13 @@
+//
+//  NotesStorage.swift
+//  WeatherNotes
+//
+//  Created by Irina Gorbachenko on 17.11.2025.
+//
+
+import Foundation
+
+class NotesStorage {
+    let userDefaults = UserDefaults.standard
+    
+}
