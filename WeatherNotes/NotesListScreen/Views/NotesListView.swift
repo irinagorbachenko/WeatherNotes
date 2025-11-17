237079d4e7e24773746e507be4812d29e925622c
Irina Gorbachenko <irinagorbachenko23@gmail.com>
2025-11-17 21:23:51 +0200

    List of notes and button implemented to NotesList views

--- WeatherNotes/NotesListScreen/Views/NotesListView.swift
+++ WeatherNotes/NotesListScreen/Views/NotesListView.swift
@@ -4,4 +4,46 @@
 //
 //  Created by Irina Gorbachenko on 17.11.2025.
 //
+import SwiftUI
 
+struct NotesListView: View {
+    @StateObject var viewModel = NoteListViewModel()
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(viewModel.notes) { note in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(note.title)
+                                .font(.title3)
+                            Text(note.formattedDate)
+                                .font(.caption)
+                        }
+                        
+                        Spacer()
+                        
+                        VStack {
+                            Text(note.formattedTemperature)
+                                .foregroundStyle(.red)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Notes")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        print("Pressed")
+                    } label: {
+                        Text("Add")
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    NotesListView()
+}
