ae78c943d129f540d15669e34dda36c24dda5319
Irina Gorbachenko <irinagorbachenko23@gmail.com>
2025-11-17 15:31:13 +0200

    Model Note is created

---
+++ WeatherNotes/NotesListScreen/Models/Note.swift
@@ -0,0 +1,14 @@
+//
+//  Note.swift
+//  WeatherNotes
+//
+//  Created by Irina Gorbachenko on 17.11.2025.
+//
+
+import Foundation
+
+struct Note: Identifiable {
+    var id: UUID = UUID()
+    
+    
+}
